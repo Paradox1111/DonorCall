24791f9ffefa6173bc30c6c8b761268cc6c55925
SRBond <skyler.bond7@gmail.com>
2020-03-06 14:26:10 -0800

    implement react-bootstrap for nav/app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Link, Route, Switch } from "react-router-dom";
 import Donors from "./components/Donors";
 import Stewards from "./components/Stewards";
@@ -7,23 +7,54 @@ import NewSteward from "./components/NewSteward";
 import NewDonor from "./components/NewDonor";
 import "./App.css";
 
+import {
+	Nav,
+	Button,
+	Row,
+	Col,
+	Container,
+	NavDropdown,
+	Navbar
+} from "react-bootstrap";
+
 function App() {
 	return (
-		<nav>
-			<nav>
-				<Link to='/'>All donors</Link>
-				<Link to='/stewards'>Stewards</Link>
-				<Link to='/stewards/new'>New steward</Link>
-				<Link to='/donors/new'>New donor</Link>
-				<Link>Login/Logout</Link>
-			</nav>
-			<Switch>
-				<Route exact path='/' component={Donors} />
-				<Route path='/stewards/new' render={NewSteward} />
-				<Route path='/stewards' component={Stewards} />
-				<Route path='/donors/new' render={NewDonor} />
-			</Switch>
-		</nav>
+		<Container>
+			<Row>
+				<Navbar>
+					<Navbar.Brand href='/'>Donor Call</Navbar.Brand>
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav'></Navbar.Collapse>
+					<Nav>
+						<Nav.Link href='/'>Donors</Nav.Link>
+
+						<Nav.Link href='/stewards'>Stewards</Nav.Link>
+
+						<NavDropdown title='New'>
+							<NavDropdown.Item href='/stewards/new'>
+								Add a steward
+							</NavDropdown.Item>
+
+							<NavDropdown.Item href='/donors/new'>
+								Add a donor
+							</NavDropdown.Item>
+						</NavDropdown>
+
+						<Nav.Link>Login/Logout</Nav.Link>
+					</Nav>
+				</Navbar>
+			</Row>
+			<Row>
+				<main>
+					<Switch>
+						<Route exact path='/' component={Donors} />
+						<Route path='/stewards/new' render={NewSteward} />
+						<Route path='/stewards' component={Stewards} />
+						<Route path='/donors/new' render={NewDonor} />
+					</Switch>
+				</main>
+			</Row>
+		</Container>
 	);
 }
 
